3fb9dcc46262fde69f2365ac6302692698b9fbb2
latoya <simbinelatifa@gmail.com>
2021-01-21 09:54:32 +0200

    task10

--- task9.java
+++ task9.java
@@ -1,3 +1,4 @@
+ import java.util.Scanner;
  public class vowels
 {
 	static void vowels(String word)
---
+++ tast10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class common
+{
+static void common(String word1, String word2)
+	{
+		for (int i = 0; i < word1.length(); i++)
+		{
+			for (int j = 0; j <word2.length(); j++)
+			{
+				if (word1.charAt(i) ==word2.charAt(j))
+				{
+					System.out.print(word1.charAt(i));
+				}
+			}
+		}
+	}
+
+
+	public static void main (String[] args)
+	{
+		common("house","computers");
+	}
+}
